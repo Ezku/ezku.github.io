532ab82a96e95c2007be907d0a21a0fa90396a7f
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-10 13:25:08 +0200

    Render index page via react

---
+++ v2/src/app.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import IndexPage from './pages/index';
+
+ReactDOM.render(<IndexPage />, document.getElementById('content'));

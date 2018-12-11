a7dc531c62da1425a3d98a3e9dca8b908368fb4d
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-11 19:31:45 +0200

    Render container with neue-haas-unica + system font stack

--- v2/src/components/layout/Container.tsx
+++ v2/src/components/layout/Container.tsx
@@ -4,5 +4,15 @@ import * as React from 'react';
 import { ReactNode } from 'react';
 
 export default function Container(props: { children: ReactNode }) {
-  return <div className="container">{props.children}</div>;
+  return (
+    <div
+      className="container"
+      style={{
+        fontFamily:
+          'neue-haas-unica, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+      }}
+    >
+      {props.children}
+    </div>
+  );
 }

1d6e7ef983f4c758499dc137ae4e576517c11758
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-11 19:45:06 +0200

    Extract font stack declaration from Container

--- v2/src/components/layout/Container.tsx
+++ v2/src/components/layout/Container.tsx
@@ -1,15 +1,14 @@
-import '../../fonts.css';
-
 import * as React from 'react';
 import { ReactNode } from 'react';
+
+import { fontStack } from '../typography/fonts';
 
 export default function Container(props: { children: ReactNode }) {
   return (
     <div
       className="container"
       style={{
-        fontFamily:
-          'neue-haas-unica, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        fontFamily: fontStack.join(', ')
       }}
     >
       {props.children}
---
+++ v2/src/components/typography/fonts.ts
@@ -0,0 +1,16 @@
+import '../../fonts.css';
+
+// See: https://css-tricks.com/snippets/css/system-font-stack/
+export const systemFontStack = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  'Helvetica',
+  'Arial',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"'
+];
+export const fontStack = ['neue-haas-unica', ...systemFontStack];

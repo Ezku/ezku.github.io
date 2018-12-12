edd1fadf464c48350dcc940d1f5c2dcdf077edfe
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-12 13:53:44 +0200

    Set font weights for headers

--- v2/src/components/Typography.tsx
+++ v2/src/components/Typography.tsx
@@ -10,18 +10,21 @@ export const Section = styled.section({
 
 export const H1 = styled.h1({
   fontFamily: headerFontStack.join(', '),
+  fontWeight: 700,
   fontSize: metrics.xl,
   lineHeight: metrics.xxl
 });
 
 export const H2 = styled.h2({
   fontFamily: headerFontStack.join(', '),
+  fontWeight: 700,
   fontSize: metrics.l,
   lineHeight: metrics.xl
 });
 
 export const H3 = styled.h3({
   fontFamily: headerFontStack.join(', '),
+  fontWeight: 600,
   fontSize: metrics.m,
   lineHeight: metrics.l
 });

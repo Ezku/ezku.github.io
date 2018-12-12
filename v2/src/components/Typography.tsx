4a8bf61316ddc0e66c75d215c597c13be1b369ca
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-12 14:17:28 +0200

    Increase header font sizes

--- v2/src/components/Typography.tsx
+++ v2/src/components/Typography.tsx
@@ -11,20 +11,20 @@ export const Section = styled.section({
 export const H1 = styled.h1({
   fontFamily: headerFontStack.join(', '),
   fontWeight: 700,
-  fontSize: metrics.xl,
+  fontSize: metrics.xxl,
   lineHeight: metrics.xxl
 });
 
 export const H2 = styled.h2({
   fontFamily: headerFontStack.join(', '),
   fontWeight: 700,
-  fontSize: metrics.l,
+  fontSize: metrics.xl,
   lineHeight: metrics.xl
 });
 
 export const H3 = styled.h3({
   fontFamily: headerFontStack.join(', '),
   fontWeight: 600,
-  fontSize: metrics.m,
-  lineHeight: metrics.l
+  fontSize: metrics.l,
+  lineHeight: metrics.xl
 });

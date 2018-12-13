d42dc01d3c09d2a4528061f7789928b973dd6815
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-13 09:16:40 +0200

    Bump horizontal margin for container

--- v2/src/components/Layout.tsx
+++ v2/src/components/Layout.tsx
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
 import { Section } from './Typography';
+import { metrics } from '../utils/typography';
 
 export const Container = styled(Section)({
   width: '61.8%',
   maxWidth: '72ch',
-  margin: '0 auto'
+  margin: `${metrics.m} auto`
 });

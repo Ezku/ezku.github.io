a0c8ed53e15d73679482a33fafbef6e24672efde
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-13 09:29:43 +0200

    Style container width for different breakpoints

--- v2/src/components/Layout.tsx
+++ v2/src/components/Layout.tsx
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled';
 import { Section } from './Typography';
 import { metrics } from '../utils/typography';
+import { breakpoints } from '../utils/layout';
 
 export const Container = styled(Section)({
-  width: '61.8%',
+  minWidth: '320px',
   maxWidth: '72ch',
-  margin: `${metrics.m} auto`
+  margin: `0 auto`,
+  padding: metrics.m,
+  [breakpoints[0]]: {
+    width: '61.8%'
+  }
 });

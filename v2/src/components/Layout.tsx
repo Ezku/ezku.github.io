81d83a2ebaf14754350100a585c9a1c7accd68e1
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-13 09:37:20 +0200

    Avoid width breakpoints in favour of stretchy box

--- v2/src/components/Layout.tsx
+++ v2/src/components/Layout.tsx
@@ -1,14 +1,12 @@
 import styled from '@emotion/styled';
 import { Section } from './Typography';
 import { metrics } from '../utils/typography';
-import { breakpoints } from '../utils/layout';
 
 export const Container = styled(Section)({
+  boxSizing: 'border-box',
   minWidth: '320px',
   maxWidth: '72ch',
   margin: `0 auto`,
   padding: metrics.m,
-  [breakpoints[0]]: {
-    width: '61.8%'
-  }
+  width: '61.8%'
 });

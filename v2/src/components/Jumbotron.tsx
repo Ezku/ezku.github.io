3af24f7b2ef2e75ca852ad101490d26e760fefa3
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-13 10:49:11 +0200

    Embetterify Jumbotron signature

--- v2/src/components/Jumbotron.tsx
+++ v2/src/components/Jumbotron.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { H1, H2, P } from './Typography';
 
+type Components = {
+  Title: React.FunctionComponent;
+  Subtitle: React.FunctionComponent;
+  Lead: React.FunctionComponent;
+};
+
 type Props = {
-  children: (
-    components: {
-      Title: any;
-      Subtitle: any;
-      Lead: any;
-    }
-  ) => React.ReactNode;
+  children: (components: Components) => React.ReactNode;
 };
 export default function Jumbotron(props: Props) {
   return (

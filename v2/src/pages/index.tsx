24d710b36b6c1c44646ef145aa2a771004230bd4
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-17 13:30:14 +0200

    Convert whole recommendations section to Recommendation elements

--- v2/src/pages/index.tsx
+++ v2/src/pages/index.tsx
@@ -460,7 +460,7 @@ export default function index() {
                     &ldquo;He’s bright, hard-working and has a both pragmatic and theoretic take on
                     his responsibilities &hellip; a vivid and enthusiastic personality &hellip;
                     super skilled developer &hellip;{' '}
-                    <Emphasis>an excellent facilitator and communicator</Emphasis> &rdquo;
+                    <Emphasis>an excellent facilitator and communicator</Emphasis>&rdquo;
                   </Quote>
                   <Author>
                     <Link href="https://www.linkedin.com/in/tomi-h%C3%A4nninen-92a15364/">
@@ -472,78 +472,92 @@ export default function index() {
               )}
             </Recommendation>
 
-            <div className="row">
-              <div className="col-md-4">
-                <blockquote>
-                  <P />
-                  <cite />
-                </blockquote>
-                <blockquote>
-                  <P>
+            <Recommendation>
+              {({ Quote, Emphasis, Author, Link }) => (
+                <div>
+                  <Quote>
                     &ldquo;Hardworking, buzzword-compliant and well focused{' '}
-                    <strong>professional with a good sense of humour</strong>. Specially good at
+                    <Emphasis>professional with a good sense of humour</Emphasis>. Specially good at
                     leading projects and helping junior developers to grow.&rdquo;
-                  </P>
-                  <cite>
-                    <a href="https://www.linkedin.com/in/jouini">Nizar Jouini</a>
+                  </Quote>
+                  <Author>
+                    <Link href="https://www.linkedin.com/in/jouini">Nizar Jouini</Link>
                     CEO, Ideapalveluksi.fi
                     <br />
                     Principal Consultant, Reaktor
-                  </cite>
-                </blockquote>
-              </div>
-              <div className="col-md-4">
-                <blockquote>
-                  <P>
+                  </Author>
+                </div>
+              )}
+            </Recommendation>
+
+            <Recommendation>
+              {({ Quote, Emphasis, Author, Link }) => (
+                <div>
+                  <Quote>
                     &ldquo;An extremely talented software developer &hellip;{' '}
-                    <strong>not just a dull techie</strong>, but social, willing to help others,
+                    <Emphasis>not just a dull techie</Emphasis>, but social, willing to help others,
                     observes and improves on how things get done&rdquo;
-                  </P>
-                  <cite>
-                    <a href="https://www.linkedin.com/in/mattipaksula">Matti Paksula</a>
+                  </Quote>
+                  <Author>
+                    <Link href="https://www.linkedin.com/in/mattipaksula">Matti Paksula</Link>
                     CTO, AppGyver
-                  </cite>
-                </blockquote>
-                <blockquote>
-                  <P>
+                  </Author>
+                </div>
+              )}
+            </Recommendation>
+
+            <Recommendation>
+              {({ Quote, Emphasis, Author, Link }) => (
+                <div>
+                  <Quote>
                     &ldquo;Solid technical skills &hellip; expertise in software development
                     methodologies and facilitation. &hellip;{' '}
-                    <strong>A strong can-do attitude</strong> &hellip; well-liked by
+                    <Emphasis>A strong can-do attitude</Emphasis> &hellip; well-liked by
                     customers.&rdquo;
-                  </P>
-                  <cite>
-                    <a href="http://www.linkedin.com/profile/view?id=3814375">Paavo Punkari</a>
+                  </Quote>
+                  <Author>
+                    <Link href="http://www.linkedin.com/profile/view?id=3814375">
+                      Paavo Punkari
+                    </Link>
                     Business Director, Futurice
-                  </cite>
-                </blockquote>
-              </div>
-              <div className="col-md-4">
-                <blockquote>
-                  <P>
+                  </Author>
+                </div>
+              )}
+            </Recommendation>
+
+            <Recommendation>
+              {({ Quote, Emphasis, Author, Link }) => (
+                <div>
+                  <Quote>
                     &ldquo;Ingenious problem solving skills and{' '}
-                    <strong>wide knowledge of different technologies</strong> &hellip; very
+                    <Emphasis>wide knowledge of different technologies</Emphasis> &hellip; very
                     passionate and enthusiastic about the work he does and strives constantly to do
                     things the right way.&rdquo;
-                  </P>
-                  <cite>
-                    <a href="http://www.linkedin.com/profile/view?id=129157140">Jetro Suni</a>
+                  </Quote>
+                  <Author>
+                    <Link href="http://www.linkedin.com/profile/view?id=129157140">Jetro Suni</Link>
                     Senior Software Specialist, Futurice
-                  </cite>
-                </blockquote>
-                <blockquote>
-                  <P>
+                  </Author>
+                </div>
+              )}
+            </Recommendation>
+
+            <Recommendation>
+              {({ Quote, Emphasis, Author, Link }) => (
+                <div>
+                  <Quote>
                     &ldquo;Sharp, has a great attitude, and is always looking for serious
                     well-thought ways to improve and perform better.{' '}
-                    <strong>Level-headed thinker</strong> &hellip; Dedicated ambition and a serious
-                    strive towards professionalism.&rdquo;
-                  </P>
-                  <cite>
-                    <a href="http://www.linkedin.com/profile/view?id=48343">Taneli Tikka</a>
+                    <Emphasis>Level-headed thinker</Emphasis> &hellip; Dedicated ambition and a
+                    serious strive towards professionalism.&rdquo;
+                  </Quote>
+                  <Author>
+                    <Link href="http://www.linkedin.com/profile/view?id=48343">Taneli Tikka</Link>
                     CEO, Soprano Brain Alliance
-                  </cite>
-                </blockquote>
-              </div>
-            </div>
+                  </Author>
+                </div>
+              )}
+            </Recommendation>
             <Divider />
           </div>
         )}

5a988f514497c39276c0510a7049a20e744def56
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-12-29 22:19:52 +0200

    Add FontAwesomeIcon component as facade

---
+++ v2/src/components/FontAwesomeIcon.tsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faChevronDoubleRight } from '@fortawesome/pro-solid-svg-icons';
+import styled from '@emotion/styled';
+
+export default styled(FontAwesomeIcon)({
+  fontSize: '0.768em'
+});
+
+export const chevronDoubleRight = faChevronDoubleRight;
